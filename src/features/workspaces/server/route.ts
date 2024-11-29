59918d97a54bd554a83d0e773cb5fdabd530dae9
Guilherme Soares <guilhermeluzsoares@gmail.com>
2024-11-29 13:13:44 +0100

    feat: 12 - creating workspace members

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,8 +2,10 @@ import {Hono} from "hono";
 import {zValidator} from "@hono/zod-validator";
 import {createWorkspaceSchema} from "@/features/workspaces/schemas";
 import {sessionMiddleware} from "@/lib/session-middleware";
-import {BUCKET_URL, DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID} from "@/config";
-import { ID } from "node-appwrite";
+import {BUCKET_URL, DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID} from "@/config";
+import {ID, Query} from "node-appwrite";
+import {MemberRole} from "@/features/members/types";
+import {generateInviteCode} from "@/lib/utils";
 
 const app = new Hono()
     .get(
@@ -11,10 +13,27 @@ const app = new Hono()
         sessionMiddleware,
         async (c) => {
             const databases = c.get("databases");
+            const user = c.get("user");
+
+            const members = await databases.listDocuments(
+                DATABASE_ID,
+                MEMBERS_ID,
+                [Query.equal("userId", user.$id)]
+            );
+
+            if (members.total === 0) {
+                return c.json({ data: { documents: [], total: 0}})
+            }
+
+            const workspaceIds = members.documents.map((member) => member.workspaceId);
 
             const workspaces = await databases.listDocuments(
                 DATABASE_ID,
-                WORKSPACES_ID
+                WORKSPACES_ID,
+                [
+                    Query.contains("$id", workspaceIds),
+                    Query.orderDesc("$createdAt")
+                ]
             );
 
             return c.json({ data: workspaces });
@@ -51,8 +70,20 @@ const app = new Hono()
                     name,
                     userId: user.$id,
                     imageUrl: uploadedImageUrl,
+                    inviteCode: generateInviteCode(10),
                 }
             );
+
+            await databases.createDocument(
+                DATABASE_ID,
+                MEMBERS_ID,
+                ID.unique(),
+                {
+                    userId: user.$id,
+                    workspaceId: workspace.$id,
+                    role: MemberRole.ADMIN,
+                }
+            )
 
             return c.json({ data: workspace });
         }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function generateInviteCode(lenght: number) {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcedfghijklmnopqrstuvwxyz0123456789-;:";
+
+  let result = "";
+
+  for (let i = 0; i < lenght; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return result;
+}
