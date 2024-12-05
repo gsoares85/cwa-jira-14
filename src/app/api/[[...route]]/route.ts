dbd8c2927f6c565b927e67637b6925170f026acc
Guilherme Soares <guilhermeluzsoares@gmail.com>
2024-12-05 20:57:09 +0100

    feat: 20 - building the members list

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,12 +2,14 @@ import {Hono} from "hono";
 import {handle} from "hono/vercel";
 import authRoutes from "@/features/auth/server/route";
 import workspacesRoutes from "@/features/workspaces/server/route";
+import membersRoutes from "@/features/members/server/route";
 
 const app = new Hono().basePath("/api");
 
 const routes = app
     .route("/auth", authRoutes)
-    .route("/workspaces", workspacesRoutes);
+    .route("/workspaces", workspacesRoutes)
+    .route("/members", membersRoutes);
 
 export const GET = handle(app);
 export const POST = handle(app);
